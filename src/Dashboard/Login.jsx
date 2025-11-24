40b4a0906566ddce64370f48c678fea29d512456
Divyanshimehra <divyanshii.mehra@gmail.com>
2025-11-24 10:31:55 +0530

    logout button

--- src/Dashboard/Login.jsx
+++ src/Dashboard/Login.jsx
@@ -1,7 +1,12 @@
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 export default function Login() {
   const navigate = useNavigate();
+
+  const isLoggedIn = localStorage.getItem("role");
+  if (isLoggedIn) {
+    return <Navigate to="/dashboard/" replace />;
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault(); //prevents page refresh
@@ -22,12 +27,12 @@ export default function Login() {
       <form className="flex flex-col justify-center gap-7" onSubmit={handleSubmit}>
         <div className="flex flex-col justify-center gap-2">
           <label className="text-gray-600 font-medium">Email</label>
-          <input className="border border-gray-500 p-2 rounder w-110" type="email" name="email" />
+          <input className="border border-gray-500 p-2 rounder w-110" type="email" name="email" required/>
         </div>
 
         <div className="flex flex-col justify-center gap-2">
           <label className="text-gray-600 font-medium">Password</label>
-          <input className="border border-gray-500 p-2 rounder w-110" type="password" name="password" />
+          <input className="border border-gray-500 p-2 rounder w-110" type="password" name="password" required />
           <h3 className="text-sky-600 text-right font-semibold">Forgot password?</h3>
         </div>
         <button className="bg-sky-600 text-white font-bold p-3 rounded w-110" type="submit">LOGIN</button>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,10 +4,18 @@ import userPic from '/src/assets/userPic.jpeg'
 import LogoutIcon from '@mui/icons-material/Logout';
 import { useContext } from 'react';
 import { AppContext } from '../Context/AppContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function NavBar({ toggleSidebar }) {
     // const userName = localStorage.getItem("username") || "User";
     const {userName} = useContext(AppContext);
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("role");
+        return navigate("/");
+    }
 
     return (
         <div className="flex items-center justify-between h-20 px-6 bg-white shadow">
@@ -22,7 +30,7 @@ export default function NavBar({ toggleSidebar }) {
                 <h3 className='text-gray-700'>
                     Welcome, <span className="font-semibold">{userName}</span>
                 </h3>
-                <button className='flex items-center gap-1 border border-blue-700 px-3 py-1 rounded text-blue-700 font-medium cursor-pointer'>
+                <button onClick={handleLogout} className='flex items-center gap-1 border border-blue-700 px-3 py-1 rounded text-blue-700 font-medium cursor-pointer'>
                     <LogoutIcon fontSize="small" />
                     Logout
                 </button>
